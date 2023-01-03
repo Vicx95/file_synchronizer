a49818bb49949fe6def2e9275807f572bdbb0424
aniolm <aniolm@o2.pl>
2023-01-03 18:01:39 +0100

    Timer implementation added

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include "..//inc/timer.hpp"
 
 int main() {
-    std::cout << "Hello from app!\n";
+    Timer tm;
+
+    tm.start(std::chrono::milliseconds(1000), [] {
+        std::puts("Synchronizuje!");
+    });
+    std::this_thread::sleep_for(std::chrono::seconds(10));
 }
