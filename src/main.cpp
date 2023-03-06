d6bad0db3b9963874884e460feacd5e3d2e29b15
Mikhael <michal.grzelczak@gmail.com>
2023-03-06 17:51:48 +0100

    Added ThreadPool

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,34 @@
 #include "..//inc/FileSynchronizer.hpp"
 #include "..//inc/Scanner.hpp"
+#include "..//inc/Serializer.hpp"
+#include "..//inc/ThreadPool.hpp"
 #include "..//inc/controller.hpp"
 #include "..//inc/model.hpp"
 #include "..//inc/timer.hpp"
 #include "..//inc/view.hpp"
-#include "..//inc/Serializer.hpp"
 
 #include <chrono> // chrono::system_clock
 #include <ctime>
+#include <future>
 #include <iostream>
 #include <sstream>
 #include <string>
 
 int main()
 {
-    
+
     Timer tm;
     View v;
     Scanner scanner;
     FileSynchronizer sync;
 
+    ThreadPool tp;
+    auto future = tp.submit([]() { return "Chuck Testa"; });
+
+    std::cout << future.get() << '\n';
+
     Model m(&tm, &sync, &scanner);
     Controller c(&v, &m);
     v.setListener(&c);
     v.run();
-    
 }
