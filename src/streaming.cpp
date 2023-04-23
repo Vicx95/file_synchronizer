907a640b86715ee6ded68adb4c72970d9ee86e2a
pawellos <pablox377@gmail.com>
2023-04-23 21:24:51 +0000

    list of applied machines

--- src/streaming.cpp
+++ src/streaming.cpp
@@ -3,16 +3,35 @@
 
 void Stream::loadStreaming()
 {
-    fs::path streamConfigPath = "/home/file_synchronizer/streamConfig.json";    
+    fs::path streamConfigPath = "/home/file_synchronizer/streamConfig.json";
+    fs::path tmpConfigPath = "/home/file_synchronizer/tmpConfig";    
+    fs::path mainDirectoryPath = "/home/file_synchronizer/mainDirectory";    
+    
     std::vector<DirsAndFiles> machines = m_serializer->deserialize(streamConfigPath);
-   
-    for(auto const &machine : machines)
+    std::vector<DirsAndFiles> rejectedMachines;
+    auto printFile = [](auto &file) { std::cout << " " << file; };
+
+    for(auto &machine : machines)
     {
         auto [dir, files] = machine;
         std::cout << dir << ": ";
-        auto printFile = [](auto &file) { std::cout << " " << file; };
         std::for_each(files.cbegin(), files.cend(), printFile);
         std::cout << "\n";   
     }
-    
+    std::cout << "List of applied streaming configuration: \n";
+        auto dirExist = [&mainDirectoryPath](auto &directory) { return !fs::exists(mainDirectoryPath / directory.first); };
+        machines.erase(std::remove_if(machines.begin(), machines.end(), dirExist), machines.end());
+
+    for(auto &machine : machines)
+    {
+        auto [dir, files] = machine;
+        auto fileExist = [&tmpConfigPath](auto &file) { return !fs::exists(tmpConfigPath / file); };
+        files.erase(std::remove_if(files.begin(), files.end(), fileExist), files.end());
+
+        std::cout << dir << ": ";
+        std::for_each(files.cbegin(), files.cend(), printFile);
+        std::cout << "\n";   
+    }
+
+
 }
