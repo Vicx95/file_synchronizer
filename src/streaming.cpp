e23e15ea66b0cce2c62dea52fd8fe0e7f6499a68
pawellos <pablox377@gmail.com>
2023-04-26 00:18:29 +0000

    Add rejected files and machines

--- src/streaming.cpp
+++ src/streaming.cpp
@@ -3,35 +3,46 @@
 
 void Stream::loadStreaming()
 {
-    fs::path streamConfigPath = "/home/file_synchronizer/streamConfig.json";
-    fs::path tmpConfigPath = "/home/file_synchronizer/tmpConfig";    
-    fs::path mainDirectoryPath = "/home/file_synchronizer/mainDirectory";    
+    fs::path streamConfigPath = "/file_synchronizer/streamConfig.json";
+    fs::path tmpConfigPath = "/file_synchronizer/tmpConfig";    
+    fs::path mainDirectoryPath = "/file_synchronizer/mainDirectory";    
     
     std::vector<DirsAndFiles> machines = m_serializer->deserialize(streamConfigPath);
-    std::vector<DirsAndFiles> rejectedMachines;
-    auto printFile = [](auto &file) { std::cout << " " << file; };
-
-    for(auto &machine : machines)
+    std::vector<DirsAndFiles> rejectedMachines = machines;
+    auto printFileName = [](auto &file) { std::cout << " " << file; };
+    
+    std::cout << "JSON streaming configuration: \n";
+    for(const auto &machine : machines)
     {
         auto [dir, files] = machine;
         std::cout << dir << ": ";
-        std::for_each(files.cbegin(), files.cend(), printFile);
+        std::for_each(files.cbegin(), files.cend(), printFileName);
         std::cout << "\n";   
     }
     std::cout << "List of applied streaming configuration: \n";
-        auto dirExist = [&mainDirectoryPath](auto &directory) { return !fs::exists(mainDirectoryPath / directory.first); };
-        machines.erase(std::remove_if(machines.begin(), machines.end(), dirExist), machines.end());
+        auto dirNotExists = [&mainDirectoryPath](auto &directory) { return !fs::exists(mainDirectoryPath / directory.first); };
+        //std::copy_if(machines.begin(), machines.end(), std::back_inserter(rejectedMachines), dirNotExists);
+        machines.erase(std::remove_if(machines.begin(), machines.end(), dirNotExists), machines.end());
 
-    for(auto &machine : machines)
+    for(const auto &machine : machines)
     {
         auto [dir, files] = machine;
-        auto fileExist = [&tmpConfigPath](auto &file) { return !fs::exists(tmpConfigPath / file); };
-        files.erase(std::remove_if(files.begin(), files.end(), fileExist), files.end());
-
+        auto fileNotExist = [&tmpConfigPath](auto &file) { return !fs::exists(tmpConfigPath / file); };
+        files.erase(std::remove_if(files.begin(), files.end(), fileNotExist), files.end());
         std::cout << dir << ": ";
-        std::for_each(files.cbegin(), files.cend(), printFile);
+        std::for_each(files.cbegin(), files.cend(), printFileName);
         std::cout << "\n";   
     }
-
-
+    std::cout << "List of rejected files and directories: \n";
+    for(const auto &machine : rejectedMachines)
+    {
+        auto [dir, files] = machine;
+        auto fileExist = [&tmpConfigPath](auto &file) { return fs::exists(tmpConfigPath / file); };
+        if (!dirNotExists(machine)){
+            files.erase(std::remove_if(files.begin(), files.end(), fileExist), files.end());
+        }
+        std::cout << dir << ": ";
+        std::for_each(files.cbegin(), files.cend(), printFileName);
+        std::cout << "\n";   
+    }
 }
